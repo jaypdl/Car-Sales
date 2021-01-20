9a123700b783af5f08e6194c20e94be973c2d2c0
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-01-19 17:10:52 -0700

    Finished MVP!!!!!

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const Total = props => {
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${props.total}</h4>
     </div>
   );
 };
 
-export default Total;
+const mapStateToProps = state => {
+  const total = state.car.price + state.additionalPrice;
+  return {
+    total: total
+  }
+} 
+
+export default connect(mapStateToProps, {})(Total);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import carSalesReducer from './reducers/carSalesReducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(
+   carSalesReducer,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+// 1. Create initial empty Reducer
+// 2. Create Redux Store
+      // import createStore
+      // import reducer
+// 3. Bring in Provider
+      // import Provider
+// 4. Connect Components
+// 5. Add Events and Event handlers
+// 6. Build Action Creatirs
+// 7. Write Reducer Logic
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+   <Provider store={store}>
+      <App />
+   </Provider>
+   , rootElement);
